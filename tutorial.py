779796ccc1ee9b367a1a59eab0737f0d64094f3f
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-10-29 15:31:05 +0300

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -16,11 +16,38 @@ print(char_name)
 phrase = "Girrafe Academy"
 print(phrase.upper()) #converts to uppercase
 print(phrase.isupper()) #return true or false
-print(len(phrase)) #returns length of stringprint ("Hello world!!!") # prints to console.
+print(len(phrase)) #returns length of stringprint 
+print(phrase[0]) #returns the char at specified index
+print(phrase.index("G")) #returns index of specified char...can also be used with words
+print(phrase.replace("Girrafe","Elephant")) #replaces first with second
 
-#variables 
-char_name = "Alpha"
-char_age = "38"
+from math import * #imports math module
 
-#using variables 
-print("My name is "+char_name+ " and am "+char_age+" years old.")
+#get input from user;
+#input("Enter your name") #alerts user to enter name ...
+
+#name = input("Enter your name: ") #use the python terminal
+#print("Hello "+name)
+
+# lists in python--- samea as array
+
+friends = ["Kevin","David","Lucky","Night",True,False]
+luck_nums  = [23,23,2,2,4,]
+print(friends[1]) #prints kevin
+print(friends[-1]) #starts counting from back so prints 46
+print(friends[1:]) #prints everything after kevin
+friends.extend(luck_nums) #appends luck_num to friends
+print(friends)
+
+friends.append("Creed") #adds to the end
+friends.insert(1, "John") # adds john at index 1
+friends.remove("Lucky") 
+friends.clear() #clears everything from the list
+friends.pop() #removes last element
+friends.index("Kevin") #says if kevin is in the list, if name isnt in list, it produces an error
+
+
+
+
+
+
