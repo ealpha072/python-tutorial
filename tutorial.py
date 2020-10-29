221f90574928b7eb468e2f76d2249b2161cc7426
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-10-29 15:02:08 +0300

    Create tutorial.py

---
+++ tutorial.py
@@ -0,0 +1,8 @@
+print ("Hello world!!!") # prints to console.
+
+#variables 
+char_name = "Alpha"
+char_age = "38"
+
+#using variables 
+print("My name is "+char_name+ " and am "+char_age+" years old.")
