d36d8af2e9c8b6bd8b9e7d5e8a4e18d6046eec8b
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-10-29 15:10:41 +0300

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -4,5 +4,23 @@ print ("Hello world!!!") # prints to console.
 char_name = "Alpha"
 char_age = "38"
 
+#using variables/concatenation 
+print("My name is "+char_name+ " and am "+char_age+" years old.")
+
+#changing variable value
+char_name = "Emmanuel"
+print(char_name)
+
+#data types --> Strings ,numbers, decimals,boolean
+#string functions
+phrase = "Girrafe Academy"
+print(phrase.upper()) #converts to uppercase
+print(phrase.isupper()) #return true or false
+print(len(phrase)) #returns length of stringprint ("Hello world!!!") # prints to console.
+
+#variables 
+char_name = "Alpha"
+char_age = "38"
+
 #using variables 
 print("My name is "+char_name+ " and am "+char_age+" years old.")
